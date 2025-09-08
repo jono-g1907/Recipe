71ef761ed91e087704fe773af472b7ef0e9e2505
jgan0018 <jgan0018@student.monash.edu>
2025-09-08 18:24:03 +1000

    Initial work

---
+++ src/enums.js
@@ -0,0 +1,23 @@
+/** @enum {string} */
+var Difficulty = {
+    EASY: 'Easy',
+    MEDIUM: 'Medium',
+    HARD: 'Hard'
+};
+  
+/** @enum {string} */
+var MealType = {
+    BREAKFAST: 'Breakfast',
+    LUNCH: 'Lunch',
+    DINNER: 'Dinner',
+    SNACK: 'Snack'
+};
+  
+/** @enum {string} */
+var Location = {
+    PANTRY: 'Pantry',
+    FRIDGE: 'Fridge',
+    FREEZER: 'Freezer'
+};
+  
+module.exports = { Difficulty: Difficulty, MealType: MealType, Location: Location };
