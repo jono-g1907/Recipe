e55e5892e5aa06b8320d47b88d19e415c876a91a
jono-g1907 <jgan0018@student.monash.edu>
2025-09-24 00:19:04 +1000

    remove

--- Recipe/config/development.js
+++
@@ -1,3 +0,0 @@
-﻿module.exports = {
-  mongodbUri: 'mongodb://localhost:27017/recipe_hub_31477046'
-};
--- Recipe/config/index.js
+++
@@ -1,9 +0,0 @@
-﻿const path = require('path');
-
-const ENV = process.env.NODE_ENV || 'development';
-const supported = new Set(['development', 'test']);
-const target = supported.has(ENV) ? ENV : 'development';
-const file = path.join(__dirname, `${target}.js`);
-const config = require(file);
-
-module.exports = config;
